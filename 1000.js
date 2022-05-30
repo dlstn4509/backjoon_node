1dcc2959c4fd6662ffbaca620838cf15dee45ec0
dlstn4509 <dlstn7609@hanmail.net>
2022-05-30 17:30:17 +0900

    [test] first commit

---
+++ 1000.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './example.txt';
+let input = fs.readFileSync(filePath).toString().split(' ');
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+console.log(a + b);
---
+++ 10869.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './example.txt';
+let input = fs.readFileSync(filePath).toString().split(' ');
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(Math.floor(a / b));
+console.log(a % b);
