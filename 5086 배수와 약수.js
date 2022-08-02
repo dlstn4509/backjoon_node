a6c505ff203971b07f84616dd1e4a6555b04e02e
dlstn4509 <dlstn7609@hanmail.net>
2022-08-02 10:23:43 +0900

    5086

--- 5086 배수와 약수.js
+++ 5086 배수와 약수.js
@@ -1,9 +1,14 @@
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './example.txt';
 let input = fs.readFileSync(filePath).toString().split('\n');
+let cnt = 0;
 
-for (let i = 0; i < input.length - 1; i++) {
-  let [a, b] = input[i].split(' ').map((v) => Number(v));
+while (true) {
+  let [a, b] = input[cnt].split(' ').map((v) => Number(v));
+  if (a === 0 && b === 0) {
+    break;
+  }
+
   if (b % a === 0) {
     console.log('factor');
   } else if (a % b === 0) {
@@ -11,4 +16,17 @@ for (let i = 0; i < input.length - 1; i++) {
   } else {
     console.log('neither');
   }
+
+  cnt += 1;
 }
+
+// for (let i = 0; i < input.length - 1; i++) {
+//   let [a, b] = input[i].split(' ').map((v) => Number(v));
+//   if (b % a === 0) {
+//     console.log('factor');
+//   } else if (a % b === 0) {
+//     console.log('multiple');
+//   } else {
+//     console.log('neither');
+//   }
+// }
