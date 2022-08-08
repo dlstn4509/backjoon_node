73d45df7895cd3fb6dbb94165149187dc224d116
dlstn4509 <dlstn7609@hanmail.net>
2022-08-08 10:20:11 +0900

    24416

--- 24416 알고리즘 수업 - 피보나치 수 1.js
+++ 24416 알고리즘 수업 - 피보나치 수 1.js
@@ -11,16 +11,32 @@ const fib = (n) => {
   }
 };
 
-// console.log(fib(num)); // 5
-
 const fibonacci = (n) => {
   let arr = [0, 1, 1];
+  let num01 = 0;
+  let num02 = 1;
+  let num03 = 1;
   let cnt = 0;
-  for (let i = 3; i <= n; i++) {
-    arr[i] = arr[i - 1] + arr[i - 2];
+  for (let i = 0; i < n; i++) {
+    num03 = num01 + num02;
+    num01 = num02;
+    num02 = num03;
     cnt += 1;
   }
+  console.log(num01);
+  console.log(num02);
+  console.log(num03);
   return cnt;
 };
+
+// const fibonacci = (n) => {
+//   let arr = [0, 1, 1];
+//   let cnt = 0;
+//   for (let i = 3; i <= n; i++) {
+//     arr[i] = arr[i - 1] + arr[i - 2];
+//     cnt += 1;
+//   }
+//   return cnt;
+// };
 
 console.log(fib(num) + ' ' + fibonacci(num));
