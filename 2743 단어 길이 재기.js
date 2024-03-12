0840b6873e1743022892c37907df79ecff60ba68
dlstn4509 <dlstn7609@hanmail.net>
2024-03-12 10:40:09 +0900

    fix

--- 2743 단어 길이 재기.js
+++ 2743 단어 길이 재기.js
@@ -2,4 +2,5 @@ const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './example.txt';
 let input = fs.readFileSync(filePath).toString().trim();
 
+
 console.log(input.length);
