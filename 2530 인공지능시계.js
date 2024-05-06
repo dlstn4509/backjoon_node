24da55711e35859b4b846013197ccd8aefe4695c
dlstn4509 <dlstn7609@hanmail.net>
2024-05-06 13:23:30 +0900

    2530

---
+++ 2530 인공지능시계.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './example.txt';
+const input = fs.readFileSync(filePath).toString().trim().split('\n');
+v = input[0].split(' ').map((x) => +x);
+h = parseInt(v[0]);
+m = parseInt(v[1]);
+s = parseInt(v[2]);
+n = parseInt(input[1]);
+sum = s + n;
+m = m + parseInt(sum / 60);
+sum %= 60;
+h = h + parseInt(m / 60);
+m %= 60;
+h %= 24;
+console.log('%d %d %d', h, m, sum);
